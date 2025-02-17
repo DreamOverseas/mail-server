48ccd9b7e6e088ee67ad8a6689eb93e7f88cf1da
Hanny <zyhaierciyuan@gmail.com>
2025-02-17 13:54:28 +1100

    Configure transporters

--- config/transporter.js
+++ config/transporter.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
-// Tencent Email SMTP settings
-const transporter = nodemailer.createTransport({
+// Tencent Email SMTP settings - Sender - Dream Overseas Group
+const transporter_send_do = nodemailer.createTransport({
   host: 'smtp.exmail.qq.com',
   port: 465,
   auth: {
@@ -10,4 +10,34 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-module.exports = transporter;
+// Tencent Email SMTP settings - Receiver - Dream Overseas Group
+const transporter_receive_do = nodemailer.createTransport({
+  host: 'imap.exmail.qq.com',
+  port: 993,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+// Tencent Email SMTP settings - Sender - 1 Club
+const transporter_send_1club = nodemailer.createTransport({
+  host: 'smtp.exmail.qq.com',
+  port: 465,
+  auth: {
+    user: process.env.SMTP_USER_1CLUB,
+    pass: process.env.SMTP_PASS_1CLUB,
+  },
+});
+
+// Tencent Email SMTP settings - Receiver - 1 Club
+const transporter_receive_1club = nodemailer.createTransport({
+  host: 'imap.exmail.qq.com',
+  port: 993,
+  auth: {
+    user: process.env.SMTP_USER_1CLUB,
+    pass: process.env.SMTP_PASS_1CLUB,
+  },
+});
+
+module.exports = {transporter_send_do, transporter_receive_do, transporter_send_1club, transporter_receive_1club};
